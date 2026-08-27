e3175ca9d49b21c54dded42ea0caab5c23b76664
Ivan.K <ivankuznetsov454@yandex.ru>
2026-08-28 00:15:28 +0500

    Initial commit

---
+++ Task1/Task1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+int** create_two_dim_array(int N) {
+    int** ptrArray{ new int* [N] {} };
+    for (int row{}; row < N; ++row) {
+        ptrArray[row] = new int[N] {};
+    }
+    return ptrArray;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int N{};
+    int** orgraph{ nullptr };
+
+    std::ifstream data("input.txt");
+    if (!data.is_open()) {
+        std::cout << "Не удалось открыть файл input.txt" << std::endl;
+        return 1;
+    }
+
+    data >> N;
+    orgraph = create_two_dim_array(N);
+    for (int i{}; i < N; ++i) {
+        for (int j{}; j < N; ++j) {
+            data >> orgraph[i][j];
+        }
+    }
+
+    data.close();
+
+    std::cout << "Текстовый вид орграфа:" << std::endl;
+    for (int i{}; i < N; ++i) {
+        bool hasArc{ false };
+
+        std::cout << i + 1 << ": ";
+        for (int j{}; j < N; ++j) {
+            if (orgraph[i][j] == 1) {
+                std::cout << j + 1 << " ";
+                hasArc = true;
+            }
+        }
+        if (!hasArc) { std::cout << "нет"; }
+
+        std::cout << std::endl;
+    }
+
+    for (int i{}; i < N; ++i) {
+        delete[] orgraph[i];
+    }
+    delete[] orgraph;
+    orgraph = nullptr;
+
+    return EXIT_SUCCESS;
+}
---
+++ Task2/Task2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <fstream>
+
+int** create_two_dim_array(int N) {
+    int** ptrArray{ new int* [N] {} };
+    for (int row{}; row < N; ++row) {
+        ptrArray[row] = new int[N] {};
+    }
+    return ptrArray;
+}
+
+void dfs(int** graph, int N, bool* visited, std::deque<int>& order, int vertex = 0) {
+    visited[vertex] = true;
+
+    for (int i{}; i < N; ++i) {
+        if (graph[vertex][i] == 1 && visited[i] == false) {
+            dfs(graph, N, visited, order, i);
+        }
+    }
+
+    order.push_front(vertex);
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int N{};
+    int** orgraph{ nullptr };
+    std::deque<int> order{};
+    bool* visited{ nullptr };
+
+    std::ifstream data("input.txt");
+    if (!data.is_open()) {
+        std::cout << "Не удалось открыть файл input.txt" << std::endl;
+        return 1;
+    }
+
+    data >> N;
+    orgraph = create_two_dim_array(N);
+    for (int i{}; i < N; ++i) {
+        for (int j{}; j < N; ++j) {
+            data >> orgraph[i][j];
+        }
+    }
+
+    data.close();
+
+    visited = new bool[N];
+    for (int i{}; i < N; ++i) {
+        visited[i] = false;
+    }
+
+    for (int i{}; i < N; ++i) {
+        if (!visited[i]) { dfs(orgraph, N, visited, order, i); }
+    }
+
+    std::cout << "Топологический порядок вершин: ";
+    for (int i{}; i < N; ++i) {
+        std::cout << order[i] + 1 << " ";
+    }
+
+    for (int i{}; i < N; ++i) {
+        delete[] orgraph[i];
+    }
+    delete[] orgraph;
+    delete[] visited;
+    orgraph = nullptr;
+    visited = nullptr;
+
+    return EXIT_SUCCESS;
+}
